62afebfdb0a339f95b3ac7fda73f02d3e3012c6a
Ryan Miller <ryanlloydmiller@gmail.com>
2025-08-25 15:23:41 -0400

    changed to work with new method for addressing loading settings

--- server/src/index_ws.ts
+++ server/src/index_ws.ts
@@ -1573,7 +1573,7 @@ async function handlePresetLoad(ip: string, clientWs: WebSocket): Promise<void> 
     }
 
     // Build a single script to ensure ordering and a single reply
-    const script = `evalNoReply {::ess::set_variant_args ${variantArgs}; ::ess::reload_variant; ::ess::set_params ${paramSettings}}`;
+    const script = `send ess {evalNoReply {::ess::set_variant_args ${variantArgs}; ::ess::reload_variant; ::ess::set_params ${paramSettings}}}`;
 
     console.log('script: ', script);
 
